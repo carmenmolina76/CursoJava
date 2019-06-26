369101c95d997c692efe1e3d82b8955ca00d8dec
soporte <soporte@ADUANA-PC>
2019-06-26 10:04:11 -0400

    subida

---
+++ ejbModule/entities/Categoria.java
@@ -0,0 +1,17 @@
+package entities;
+
+public class Categoria {
+	
+	public String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+
+}
